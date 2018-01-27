829859395d5cd8a39519e3b20067de66ba497c6a
danielo515 <rdanielo@gmail.com>
2018-01-27 16:58:58 +0100

    feat(utils): Added an utility for creating sets
    Provide an interface for different set sizes
    Make use of caching
    Smaller sets are lazy

---
+++ src/makeSet.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const defaultMaker = ()=> Math.random() * 1000
+
+module.exports = (maker = defaultMaker , setSize = 1e5)=> { 
+    
+    const bigSet = [];
+    
+    while (setSize){
+        bigSet.push( maker() );
+        setSize--;
+    }
+
+    const mediumLength = bigSet.length / 2;
+    const smallLength = mediumLength / 10;
+    const ssLength = smallLength / 10;
+
+    let medium,small,xSmall;
+    return {
+        bigSet,
+        get mediumSet(){
+            medium = medium || bigSet.slice(0,mediumLength)
+            return medium
+        },
+        get smallSet(){
+            small = small || bigSet.slice(0,smallLength)            
+            return small
+        },
+        get xSmallSet(){
+            xSmall = xSmall || bigSet.slice(0,ssLength)
+            return xSmall
+        }
+    }
+}
