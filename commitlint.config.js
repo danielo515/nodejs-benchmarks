69f4c2fdd4d36086d7cde1930bffbc64a545ad39
Danielo Rodriguez Rivero <rdanielo@gmail.com>
2018-02-02 18:08:44 +0100

    build(ci): added travis CI configuration

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,7 @@
+'use strict';
 module.exports = {
     extends: ['@commitlint/config-conventional'],
     rules: {
-        'scope-enum': [2, 'always', ['utils','benchmarks','profiling','tools','deps','instructions','charting']]
+        'scope-enum': [2, 'always', ['utils', 'benchmarks', 'profiling', 'tools', 'deps', 'instructions', 'charting', 'ci']]
     }
 };
