7e7517c0c41b3e17a725e8b8ed6ae9a44bba82fa
anonimous <nobody@nowhere.com>
2018-08-01 09:58:03 +0200

    Just another benchmark for our growing collection

---
+++ src/benchmarks/shape-validation-1533110283610.js
@@ -0,0 +1,39 @@
+'use strict';
+const joi = require ('joi');
+const { conforms, isNumber, isString } = require ('lodash');
+
+const user = {
+    name: 'Gordo'
+    , surname: 'Fat'
+    , sex: 'male'
+    , email: 'you@me.com'
+    , age: 22
+};
+module.exports = (suite, benchmark) => {
+    
+    suite(`Validating a simple object`, () => {
+
+        benchmark(`Joi`, () => {
+
+            Joi.object().keys({
+                name: Joi.string(),
+                surname: Joi.string(),
+                sex: Joi.string(),
+                email: Joi.string(),
+                age: Joi.number()
+            }).validate(user);
+        });
+        benchmark(`Lodash conforms`, () => {
+
+            conforms({
+                name: isString,
+                surname: isString,
+                sex: isString,
+                email: isString,
+                age: isNumber
+            });
+        });
+
+    });
+
+};
