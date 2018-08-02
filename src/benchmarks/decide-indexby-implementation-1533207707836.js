81ddb0d877aa5bc94d607826434b2a809298c0ff
Danielo Rodriguez <rdanielo@gmail.com>
2018-08-02 13:14:40 +0200

    feat(benchmarks): added benchmark with more users

--- src/benchmarks/decide-indexby-implementation-1533207707836.js
+++ src/benchmarks/decide-indexby-implementation-1533207707836.js
@@ -1,18 +1,20 @@
 'use strict';
-const { singleton, ap, flip, map, prop, compose, insert, foldMap } = require ('sanctuary');
-
+const { singleton, ap, flip, map, prop, compose, insert, foldMap, pipe } = require ('sanctuary');
+const { users: Users } = require('../fake-data');
 const users = [{name:'joe', age: 12 },{name:'peter', age:15}];
 const B = compose;
 const indexBy = function (key) {
     return flip(B(pipe)(map(ap(flip(insert))(prop(key)))))({});
 };
-function ixBy(f) {
-    return foldMap (Object) (ap (flip (singleton)) (f));
+function ixBy(key) {
+    return foldMap (Object) (ap (flip (singleton)) (prop(key)));
 }
+
+const smallList = Users().smallSet;
 
 module.exports = (suite, benchmark) => {
     
-    suite(`undefined`, () => {
+    suite(`Decide indexby implementation (2 users)`, () => {
 
         benchmark(`Current implementation (pipe, map, ap and insert)`, () => {
 
@@ -20,7 +22,19 @@ module.exports = (suite, benchmark) => {
         });
         benchmark(`New implementation ( foldMap, ap and singleton)`, () => {
 
-            ixBy (prop ('name')) (users);
+            ixBy ('name') (users);
+        });
+
+    });
+    suite(`Decide indexby implementation (${smallList.length})`, () => {
+
+        benchmark(`Current implementation (pipe, map, ap and insert)`, () => {
+
+            indexBy ('name') (smallList);
+        });
+        benchmark(`New implementation ( foldMap, ap and singleton)`, () => {
+
+            ixBy ('name') (smallList);
         });
 
     });
